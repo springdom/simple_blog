65ab69d01eb8f810282acd53c163cb97d68dfdea
Matthew Taylor <taylomj@protonmail.com>
2024-06-30 10:50:31 -0400

    a

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, flash, redirect, url_for
-from app import app, db
+from flask import render_template, flash, redirect, url_for, request, current_app as app
+from app import db
 from app.forms import LoginForm, RegistrationForm, PostForm
 from app.models import User, Post
 from flask_login import current_user, login_user, logout_user, login_required

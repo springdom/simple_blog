59426d15b7218512359a5e21e164f74fc300d88b
matthew taylor <matthewtaylor@email>
2024-07-01 20:04:51 +0200

    correct

--- config.py
+++ config.py
@@ -2,5 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://matthew:J%40maica1992@dbserver/mydatabase'
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+mysqlconnector://{os.environ.get('DB_USERNAME')}:"
+        f"{os.environ.get('DB_PASSWORD')}@"
+        f"{os.environ.get('DB_HOST')}/"
+        f"{os.environ.get('DB_NAME')}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
